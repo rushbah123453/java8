4501ee26bba8361e40ef6935e12362f520279261
rushbah123453 <rushabhkumar.oswal@tomtom.com>
2021-06-13 21:33:24 +0530

    Constructor Reference

---
+++ src/methodNConstructorReference/coding/ConstructorReference.java
@@ -0,0 +1,17 @@
+package methodNConstructorReference.coding;
+
+import functionalInterfaces.coding.data.Student;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public class ConstructorReference
+{
+
+    static Supplier<Student> studentSupplier=Student::new;
+    static Function<String,Student> studentFunction=Student::new;
+    public static void main(String[] args) {
+        System.out.println(studentSupplier.get());
+        System.out.println(studentFunction.apply("student"));
+    }
+}
---
+++ src/methodNConstructorReference/theory/ConstructorReference.md
@@ -0,0 +1,4 @@
+Constructor Reference
+
+1. Syntax : className::new
+2. class of className should have a constructor

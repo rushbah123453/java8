426fb6b5f3ddfc8b9bfefccc4da1b291cc5140d1
rushbah123453 <rushabhkumar.oswal@tomtom.com>
2021-06-13 14:49:59 +0530

    Supplier Functional Interface

---
+++ src/functionalInterfaces/coding/SupplierInterfaceExample.java
@@ -0,0 +1,21 @@
+package functionalInterfaces.coding;
+
+import functionalInterfaces.coding.data.Student;
+import functionalInterfaces.coding.data.StudentDataBase;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class SupplierInterfaceExample {
+
+    public static void main(String[] args) {
+        Supplier<Student> supplier=()->{
+            return new Student("Adam",2,3.6, "male", Arrays.asList("swimming", "basketball","volleyball"));
+        };
+        System.out.println(supplier.get());
+
+        Supplier<List<Student>> listSupplier=()-> StudentDataBase.getAllStudents();
+        System.out.println(listSupplier.get());
+    }
+}

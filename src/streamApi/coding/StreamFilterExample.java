8f92af494e33bdb8d1dd836b78ac5f62b1dbfdfa
rushbah123453 <rushabh123453@gmail.com>
2021-06-26 13:44:45 +0530

    filter added

---
+++ src/streamApi/coding/StreamFilterExample.java
@@ -0,0 +1,23 @@
+package streamApi.coding;
+
+import functionalInterfaces.coding.data.Student;
+import functionalInterfaces.coding.data.StudentDataBase;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamFilterExample {
+
+    public static void main(String[] args) {
+
+        filterStudent().forEach(System.out::println);
+    }
+
+    public static List<Student> filterStudent()
+    {
+        return StudentDataBase.getAllStudents().stream()
+                .filter(student -> student.getGender().equals("male"))
+                .collect(Collectors.toList());
+
+    }
+}
